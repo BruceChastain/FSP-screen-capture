2de4b35faf7a4e58cecb88b75806cb728da00ac9
BruceChastain <alfredchastain@gmail.com>
2014-03-10 23:05:14 +0100

    First commit

---
+++ FSP screen capture.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+import datetime
+import sys
+import os
+import shutil
+from PyQt4.QtGui import QPixmap, QApplication
+
+# ---- Tell the user it's working
+print("working...")
+
+# ---- Grab the screen shot
+app = QApplication(sys.argv)
+now = datetime.datetime.now()
+date_and_time = now.strftime("%Y_%m_%d_%H_%M_%S")
+QPixmap.grabWindow(QApplication.desktop().winId()).save(date_and_time, 'png')
+
+# ---- Create he screencaps folder if needed and copy the captures to the folder
+folder = "screencaps"
+src = date_and_time
+
+if not os.path.exists(folder):
+    os.mkdir( folder, 0755 );
+    print("Folder created")   
+if os.path.exists(folder):
+    shutil.move(src, folder)
